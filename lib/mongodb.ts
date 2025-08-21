086018e4414a78e4493def0ee3b3814c7abc4610
manish091102 <manish091102@gmail.com>
2025-08-21 12:44:52 +0530

    new gau seva

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/gau-seva'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://manish091102:gauseva@cluster0.sij3u6j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
